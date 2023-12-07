1cb416f5644383f7f17487833d511a4850ab79e4
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2023-12-07 22:09:57 +0100

    add day 6 + 7 skeleton and data

---
+++ day07/src/main.rs
@@ -0,0 +1,111 @@
+use std::borrow::Cow;
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum AOCError {
+    #[error("Failed to read input: {path:?}")]
+    IOError {
+        source: std::io::Error,
+        path: Option<PathBuf>,
+    },
+
+    #[error("Failed to parse input {msg}")]
+    #[allow(unused)]
+    ParseError { msg: Cow<'static, str> },
+
+    #[error("This part of the puzzle is not yet implemented")]
+    #[allow(unused)]
+    NotYetSolved,
+}
+
+type AOCResult<T> = Result<T, AOCError>;
+
+#[derive(Clone, Debug)]
+struct Data {
+    items: Vec<String>,
+    //items: Vec<u64>,
+}
+
+impl FromStr for Data {
+    type Err = AOCError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let items = input.lines().map(|l| l.to_owned()).collect();
+        //.map(|l| l.parse::<u64>())
+        //.collect::<Result<_, _>>()
+        //.map_err(|_e| AOCError::ParseError { msg: "...".into() })?;
+
+        Ok(Data { items })
+    }
+}
+
+trait FromFile<D: FromStr<Err = AOCError>> {
+    fn from_file(path: impl AsRef<Path>) -> AOCResult<D> {
+        let path = path.as_ref();
+        fs::read_to_string(path)
+            .map_err(|source| AOCError::IOError {
+                source,
+                path: Some(path.into()),
+            })?
+            .parse::<D>()
+    }
+}
+
+impl<D: FromStr<Err = AOCError>> FromFile<D> for D {}
+
+fn part1(data: &Data) -> AOCResult<i64> {
+    Err(AOCError::NotYetSolved)
+}
+
+fn part2(data: &Data) -> AOCResult<i64> {
+    Err(AOCError::NotYetSolved)
+}
+
+fn main() -> AOCResult<()> {
+    let mut input_file = std::env::current_dir().map_err(|e| AOCError::IOError {
+        source: e,
+        path: None,
+    })?;
+    input_file.push("day07");
+    input_file.push("data");
+    input_file.push("input.txt");
+
+    let data = Data::from_file(input_file)?;
+    println!("Part 1: {}", part1(&data)?);
+    println!("Part 2: {}", part2(&data)?);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! aoc_test {
+        (
+            $func:ident,
+            $datapath:literal,
+            $dtype:ty,
+            $compute:path,
+            $expected:expr
+            $(,)?  // allow (optional) trailing comma
+        ) => {
+            #[test]
+            fn $func() -> AOCResult<()> {
+                match $compute(&<$dtype>::from_file($datapath)?) {
+                    Ok(result) => assert_eq!(result, $expected),
+                    Err(AOCError::NotYetSolved) => {}
+                    Err(e) => return Err(e),
+                };
+
+                Ok(())
+            }
+        };
+    }
+
+    aoc_test!(part1, "data/test1.txt", Data, super::part1, 0);
+    aoc_test!(part2, "data/test1.txt", Data, super::part2, 0);
+}
