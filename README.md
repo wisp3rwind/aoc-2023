e68a285255075053d9dd2dfad06f5983ae4e56ee
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2023-12-09 11:15:42 +0100

    complete day 7

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ My attempts at the Advent of Code '23, using over-engineered Rust solutions.
 - ideas for part 2:
     - bisection
     - directly solve (need to deal with the fact that the solution is an integer, though -> added a bunch of assertions to be sure there's no off-by-one error)
+
+## Day 7
+- learned about the `HashMap.entry()` API for reasonably concise insert-or-update
+- code really blown up by just duplicating everything for part 2, but didn't bother to clean this up
+- in general, things could probably by shortened a lot by depending on lexsorting of tuples even more
--- day07/src/main.rs
+++ day07/src/main.rs
@@ -41,14 +41,17 @@ struct Hand {
     hand: [u8; 5],
 }
 
+#[derive(Clone, Debug)]
+struct HandWithJokers {
+    bid: u32,
+    hand: [u8; 5],
+}
+
+
 impl Hand {
     fn typ(&self) -> HandType {
         let mut counts: HashMap<u8, u8> = HashMap::new();
-        self.hand.iter().for_each(|c| { counts.insert(*c, 0); });
-        self.hand.iter().for_each(|c| {
-            let count = counts.get_mut(c).unwrap();
-            *count += 1;
-        });
+        self.hand.iter().for_each(|c| { *counts.entry(*c).or_insert(0) += 1; });
 
         match counts.values().copied().max().unwrap() {
             5 => HandType::FiveOfAKind,
@@ -73,6 +76,70 @@ impl Hand {
     }
 }
 
+// Could probably simplify this (i.e. re-use Hand.typ) by actually replacing
+// J with the appropriate card (which should always be the most frequent one
+// among the others)
+impl HandWithJokers {
+    fn typ(&self) -> HandType {
+        let mut counts: HashMap<u8, u8> = HashMap::new();
+        self.hand.iter().for_each(|c| { *counts.entry(*c).or_insert(0) += 1; });
+
+        let jack_count = counts.get(&1);
+        match counts.values().copied().max().unwrap() {
+            5 => HandType::FiveOfAKind,
+            4 => {
+                match jack_count {
+                    Some(4) => HandType::FiveOfAKind,
+                    Some(1) => HandType::FiveOfAKind,
+                    _ => HandType::FourOfAKind,
+                }
+            },
+            3 => {
+                if let Some(_) = counts.values().find(|c| **c == 2) {
+                    match jack_count {
+                        Some(3) => HandType::FiveOfAKind,  // 3 J + 1 pair
+                        Some(2) => HandType::FiveOfAKind,  // 2 J + triplett
+                        None => HandType::FullHouse,  // no j, but 2 + 3
+                        _ => unreachable!(),
+                    }
+                } else {
+                    match jack_count {
+                        Some(3) => HandType::FourOfAKind,  // triplett of J + 2 single
+                        Some(1) => HandType::FourOfAKind, // triplett + single J
+                        None => HandType::ThreeOfAKind,  // triplett + 2 single
+                        _ => unreachable!(),
+                    }
+                }
+            },
+            2 => {
+                if counts.values().filter(|c| **c == 2).count() == 2 {
+                    match jack_count {
+                        Some(2) => HandType::FourOfAKind,  // 2 pairs, one of which J
+                        Some(1) => HandType::FullHouse,  // 2 pairs + 1 J
+                        None => HandType::TwoPair,  // just 2 pairs
+                        _ => unreachable!(),
+                    }
+                } else {
+                    match jack_count {
+                        Some(2) => HandType::ThreeOfAKind,  // 1 pair of J, 3 single
+                        Some(1) => HandType::ThreeOfAKind,  // 1 pair, 1 J, 2 other single
+                        None => HandType::OnePair,  // 1 pair, 3 single
+                        _ => unreachable!(),
+                    }
+                }
+            },
+            1 => {
+                match jack_count {
+                    Some(1) => HandType::OnePair,  // singles, 1 of which J
+                    None => HandType::HighCard,  // single cards only
+                    _ => unreachable!(),
+                }
+            },
+            _ => unreachable!(),
+        }
+    }
+}
+
 impl PartialEq for Hand {
     fn eq(&self, other: &Self) -> bool {
         self.hand == other.hand
@@ -91,22 +158,39 @@ impl PartialOrd for Hand {
             return Some(Ordering::Greater);
         }
 
-        // same type => lexicographic sort
-        //for (cs, co) in self.hand.iter().zip(other.hand.iter()) {
-            //if cs < co {
-                //return Some(Ordering::Less);
-            //} else if cs > co {
-                //return Some(Ordering::Less);
-            //}
-        //}
-
-        //return Some(Ordering::Equal);
-        
         self.hand.partial_cmp(&other.hand)
     }
 }
 
 impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl PartialEq for HandWithJokers {
+    fn eq(&self, other: &Self) -> bool {
+        self.hand == other.hand
+    }
+}
+
+impl Eq for HandWithJokers { }
+
+impl PartialOrd for HandWithJokers {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let ts = self.typ();
+        let to = other.typ();
+        if ts < to {
+            return Some(Ordering::Less);
+        } else if ts > to {
+            return Some(Ordering::Greater);
+        }
+
+        self.hand.partial_cmp(&other.hand)
+    }
+}
+
+impl Ord for HandWithJokers {
     fn cmp(&self, other: &Self) -> Ordering {
         self.partial_cmp(other).unwrap()
     }
@@ -136,6 +220,30 @@ fn read_part1(input: &str) -> AOCResult<Vec<Hand>> {
         .collect())
 }
 
+fn read_part2(input: &str) -> AOCResult<Vec<HandWithJokers>> {
+    Ok(input.lines()
+        .map(|l| {
+            let (hand_str, bid) = l.split_once(' ').unwrap();
+            let bid = bid.parse().unwrap();
+            let mut hand = [0u8; 5];
+            for (i, c) in hand_str.chars().enumerate() {
+                let c = match c {
+                    '2'..='9' => (c as u8 - '2' as u8) as u8 + 2,
+                    'T' => 10,
+                    'J' => 1,
+                    'Q' => 12,
+                    'K' => 13,
+                    'A' => 14,
+                    _ => panic!("invalid card"),
+                };
+                hand[i] = c;
+            }
+
+            HandWithJokers { bid, hand }
+        })
+        .collect())
+}
+
 fn load_input(path: impl AsRef<Path>) -> AOCResult<String> {
     let path = path.as_ref();
     fs::read_to_string(path)
@@ -154,8 +262,13 @@ fn part1(data: &mut [Hand]) -> AOCResult<u64> {
     }).sum::<u64>())
 }
 
-fn part2(data: &[Hand]) -> AOCResult<i64> {
-    Err(AOCError::NotYetSolved)
+fn part2(data: &mut [HandWithJokers]) -> AOCResult<u64> {
+    data.sort_unstable();
+    //dbg!(&data);
+
+    Ok(data.iter().enumerate().map(|(rank, hand)| {
+        (hand.bid as u64) * (rank as u64 + 1)
+    }).sum::<u64>())
 }
 
 fn main() -> AOCResult<()> {
@@ -172,7 +285,8 @@ fn main() -> AOCResult<()> {
     let mut data1 = read_part1(&input)?;
     println!("Part 1: {:?}", part1(&mut data1)?);
 
-    println!("Part 2: {}", part2(&data1)?);
+    let mut data2 = read_part2(&input)?;
+    println!("Part 2: {}", part2(&mut data2)?);
 
     Ok(())
 }
@@ -205,5 +319,5 @@ mod test {
     }
 
     aoc_test!(part1, "data/test1.txt", read_part1, super::part1, 6440);
-    aoc_test!(part2, "data/test1.txt", read_part1, super::part2, 5905);
+    aoc_test!(part2, "data/test1.txt", read_part2, super::part2, 5905);
 }
